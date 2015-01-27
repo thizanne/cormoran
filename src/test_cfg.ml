f03daf92847cba7ae6fa92ca916695f9f30ff90a
Thibault Suzanne <thi.suzanne@gmail.com>
2015-01-27 16:52:09 +0100

    Adds a CFG test which print the Dot output of the CFG of a file

---
+++ src/test_cfg.ml
@@ -0,0 +1,29 @@
+open Error
+open Printf
+
+let use_litmus = ref false
+
+let speclist = [
+  "--litmus", Arg.Set use_litmus, "Use litmus syntax";
+]
+
+(*
+(* Useless with only one arg in the speclist *)
+let speclist =
+  speclist
+  |> List.map (fun (a, b, c) -> (a, b, " " ^ c))
+  |> Arg.align
+*)
+
+let print_cfg file =
+  try
+    let lexbuf = Lexing.from_channel @@ open_in file in
+    let program, _cond = Parse.parse use_litmus lexbuf in
+    let g = Cfg.make program in
+    Cfg.Dot.output_graph stdout g
+  with
+  | Error li -> List.iter (fun e -> print_endline (msg_of_error e ^ "\n")) li
+
+let () =
+  Arg.parse speclist print_cfg
+    "Prints the Control Flow Graph of a program. Options available:"
