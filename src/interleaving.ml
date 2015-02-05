493168603e16d6341d15363da84c8a7f4330bdb5
Thibault Suzanne <thi.suzanne@gmail.com>
2015-02-05 16:36:13 +0100

    Adds Interleaving analyze module

---
+++ src/interleaving.ml
@@ -0,0 +1,13 @@
+module Make (D : Domain.Domain) =
+  Graph.Fixpoint.Make (Cfg.G)
+    (struct
+      type vertex = Cfg.G.vertex
+      type edge = Cfg.G.edge
+      type g = Cfg.G.t
+      type data = D.t
+      let direction = Graph.Fixpoint.Forward
+      let equal = ( = )
+      let join = D.union
+      let analyze (_, {Cfg.E.thread; ins}, _) d =
+        D.transfer d thread ins
+    end)
