df355775c8fe1039e8ff6790ca43f97e2ff643c2
Thibault Suzanne <thi.suzanne@gmail.com>
2015-02-05 19:28:01 +0100

    Makes Interleaving export the result as a first class module

--- src/interleaving.ml
+++ src/interleaving.ml
@@ -1,13 +1,20 @@
-module Make (D : Domain.Domain) =
-  Graph.Fixpoint.Make (Cfg.G)
-    (struct
-      type vertex = Cfg.G.vertex
-      type edge = Cfg.G.edge
-      type g = Cfg.G.t
-      type data = D.t
-      let direction = Graph.Fixpoint.Forward
-      let equal = ( = )
-      let join = D.union
-      let analyze (_, {Cfg.E.thread; ins}, _) d =
-        D.transfer d thread ins
-    end)
+module Make (D : Domain.Domain) = struct
+  module Fixpoint = Graph.Fixpoint.Make (Cfg.G)
+      (struct
+        type vertex = Cfg.G.vertex
+        type edge = Cfg.G.edge
+        type g = Cfg.G.t
+        type data = D.t
+        let direction = Graph.Fixpoint.Forward
+        let equal = ( = )
+        let join = D.union
+        let analyze (_, {Cfg.E.thread; ins}, _) d =
+          D.transfer d thread ins
+      end)
+
+  let make_result g =
+    (module struct
+       module Domain = D
+       let data = Fixpoint.analyze (fun _ -> D.empty) g
+     end : Analysis.Result)
+  end
