7bfd74c072451eaebf55a6ea7b6a1adc00bf8dd3
Thibault Suzanne <thi.suzanne@gmail.com>
2015-02-05 18:35:20 +0100

    Use [@@deriving show] to get string from error message

--- src/error.ml
+++ src/error.ml
@@ -5,19 +5,14 @@ type t =
   | SyntaxError
   | TypeError
   | NameError
+      [@@deriving show]
 
 exception Error of (t * Lexing.position * Lexing.position * string) list
-
-let name_of_error = function
-  | LexingError -> "Lexing error"
-  | SyntaxError -> "Syntax error"
-  | TypeError -> "Type error"
-  | NameError -> "Name error"
 
 let msg_of_error (e, startpos, endpos, msg) =
   let open Lexing in
   sprintf "%s:\n\tFrom line %d, column %d to line %d, column %d\n\t%s"
-    (name_of_error e)
+    (show e)
     startpos.pos_lnum (startpos.pos_cnum - startpos.pos_bol)
     endpos.pos_lnum (endpos.pos_cnum - endpos.pos_bol)
     msg
