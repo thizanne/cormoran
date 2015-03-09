0bc5b9cb58340e8d3341c6a65bbd056134ce7b39
Thibault Suzanne <thi.suzanne@gmail.com>
2015-03-09 17:59:42 +0100

    Updates Error
    Better interface for the error type and exception + helper functions

--- src/error.ml
+++ src/error.ml
@@ -5,7 +5,7 @@ see https://github.com/ocaml-batteries-team/batteries-included/issues/597
 
 open Printf
 
-type t =
+type error =
   | LexingError
   | SyntaxError
   | TypeError
@@ -13,12 +13,54 @@ type t =
   | NotImplementedError
       [@@deriving show]
 
-exception Error of (t * Lexing.position * Lexing.position * string) list
+type t = {
+  error : error;
+  err_loc : Location.t;
+  err_msg : string;
+}
 
-let msg_of_error (e, startpos, endpos, msg) =
+exception Error of t
+
+let on_item_error error item err_msg =
+  raise @@ Error {error; err_loc = item.Location.loc; err_msg}
+
+let lexing_error item err_msg =
+  on_item_error LexingError item err_msg
+
+let syntax_error item err_msg =
+  on_item_error SyntaxError item err_msg
+
+let type_error item err_msg =
+  on_item_error TypeError item err_msg
+
+let name_error item err_msg =
+  on_item_error NameError item err_msg
+
+let not_implemented_error item err_msg =
+  on_item_error NotImplementedError item err_msg
+
+let msg_error error err_msg =
+  raise @@ Error {error; err_loc = Location.dummy; err_msg}
+
+let lexing_msg_error err_msg =
+  msg_error LexingError err_msg
+
+let syntax_msg_error err_msg =
+  msg_error SyntaxError err_msg
+
+let type_msg_error err_msg =
+  msg_error TypeError err_msg
+
+let name_msg_error err_msg =
+  msg_error NameError err_msg
+
+let not_implemented_msg_error err_msg =
+  msg_error NotImplementedError err_msg
+
+let to_string {error; err_loc = {Location.startpos; endpos}; err_msg} =
   let open Lexing in
   sprintf "%s:\n\tFrom line %d, column %d to line %d, column %d\n\t%s"
-    (show e)
+    (show_error error)
     startpos.pos_lnum (startpos.pos_cnum - startpos.pos_bol)
     endpos.pos_lnum (endpos.pos_cnum - endpos.pos_bol)
-    msg
+    err_msg
