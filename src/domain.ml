a911c91b52e6af4f419cb97a7d9632d1d1c68656
Thibault Suzanne <thi.suzanne@gmail.com>
2015-03-11 20:40:22 +0100

    Updates Domain according to the new interfaces

--- src/domain.ml
+++ src/domain.ml
@@ -10,6 +10,8 @@ module type Outer = sig
   val transfer : t -> Flow.Operation.t -> t
   val join : t -> t -> t
 
+  (* This is only for compatibility reasons with some old code, and
+     will be removed with a proper property verification *)
   val satisfies : (string * int) list -> t -> bool
 
   val print : 'a IO.output -> t -> unit
@@ -29,17 +31,17 @@ module type Inner = sig
 
   val join_array : t array -> t
 
-  val meet_cons : t -> Constraint.t -> t
-  val meet_cons_array : t -> Constraint.t array -> t
+  val meet_cons : t -> Syntax.condition -> t
+  val meet_cons_array : t -> Syntax.condition array -> t
 
   val assign_expr :
     t -> ?dest:t option ->
-    Expression.var -> Expression.t ->
+    Syntax.var -> Syntax.expression ->
     t
 
   val assign_expr_array :
     t -> ?dest: t option ->
-    Expression.var array -> Expression.t array ->
+    Syntax.var array -> Syntax.expression array ->
     t
 
   val print : 'a IO.output -> t -> unit
