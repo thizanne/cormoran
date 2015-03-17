bfdd26b2dcdc14e8da4433e8a5c8d5b47f47eba4
Thibault Suzanne <thi.suzanne@gmail.com>
2015-03-17 22:28:26 +0100

    Renames Syntax to Program - remainers

--- src/analysis.ml
+++ src/analysis.ml
@@ -2,7 +2,7 @@ open Batteries
 
 module type Result = sig
   module Domain : Domain.Outer
-  val data : Syntax.control_state -> Domain.t
+  val data : Program.control_state -> Domain.t
 end
 
 module EmptyResult (D : Domain.Outer) : Result = struct
