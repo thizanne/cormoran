f8e3ed0cc7ed38bb0d5d3efc2b93d7b4504916a3
Thibault Suzanne <thi.suzanne@gmail.com>
2015-04-13 17:54:08 +0200

    Fixes canal binary and Param

--- src/canal.ml
+++ src/canal.ml
@@ -9,27 +9,21 @@ let info =
   ] in
   Term.info "canal" ~version:"0.1" ~doc ~man
 
-let make_analysis d =
-  let module D = (val d : Domain.Outer) in
-  let module A = Interleaving.Make (D) in
-  A.make_analyze
-
-let () =
-  let module P = Param in
-  let open P.CommandTerm in
-  let open Term in
-  let program = P.Parse.parse $ use_litmus $ filename in
-  let program = pure fst $ program in
-  let g = pure Cfg.of_program $ program in
-  let d = P.Domain.get $ domain in
+let main domain widening_delay use_litmus filename =
+  let open Param in
+  let (program, _) = Parse.parse_filename use_litmus filename in
+  let g = Cfg.of_program program in
+  let module D = (val Domain.get domain) in
   let module Analysis = Interleaving.Make (D) in
   let analyze = Analysis.make_analyze g in
   let module Result = struct module Domain = D let data = analyze end in
   let module Dot = ExportCfg.Dot (Result) in
   Dot.output_graph Legacy.stdout g.Cfg.graph
 
-let () =
-  match Term.eval (Term.(pure failwith $ pure "lol"), info) with
+let main_term =
+  let open Param.CommandTerm in
+  Term.(pure main $ domain $ widening_delay $ use_litmus $ filename)
+
+let () = match Term.eval (main_term, info) with
   | `Error _ -> exit 1
-  | `Ok s -> print_string s
   | _ -> exit 0
--- src/param.mli
+++ src/param.mli
@@ -1,16 +1,15 @@
-module CommandTerm : sig
-  val domain : [> `Concrete | `Oct | `Order | `Polka | `Top ] Cmdliner.Term.t
-  val widening_delay : int Cmdliner.Term.t
-  val use_litmus : bool Cmdliner.Term.t
-  val filename : string Cmdliner.Term.t
+module Domain : sig
+  type t
+  val get : t -> (module Domain.Outer)
 end
 
 module Parse : sig
-  val parse : (bool -> string -> Program.t * 'a list) Cmdliner.Term.t
+  val parse_filename : bool -> string -> Program.t * 'a list
 end
 
-module Domain : sig
-  val get :
-    ([< `Concrete | `Oct | `Order | `Polka | `Top ] -> (module Domain.Outer))
-      Cmdliner.Term.t
+module CommandTerm : sig
+  val domain : Domain.t Cmdliner.Term.t
+  val widening_delay : int Cmdliner.Term.t
+  val use_litmus : bool Cmdliner.Term.t
+  val filename : string Cmdliner.Term.t
 end
