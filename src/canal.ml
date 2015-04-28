34d5c6007a65edbd4f43e24fbcb90253358a2462
Thibault Suzanne <thi.suzanne@gmail.com>
2015-04-28 16:09:33 +0200

    Makes canal catch Error.Error

--- src/canal.ml
+++ src/canal.ml
@@ -10,18 +10,22 @@ let info =
   Term.info "canal" ~version:"0.1" ~doc ~man
 
 let main domain widening_delay use_litmus sourcefile outputfile =
-  let module P = Param in
-  let (program, _) = P.Parse.parse_filename use_litmus sourcefile in
-  let g = Cfg.of_program program in
-  let module D = (val P.Domain.get domain) in
-  let module Analysis = Interleaving.Make (D) in
-  let data = Analysis.analyze g widening_delay in
-  let module Dot = ExportCfg.Dot (D) in
-  let module Prop = Property.Make (D) in
-  Dot.output_graph (P.Output.get_output outputfile) data g;
-  if Prop.satisfies g data
-  then print_endline "Property verified."
-  else print_endline "Property could not be verified: either it is wrong, or the domain is too imprecise."
+  try
+    let module P = Param in
+    let (program, _) = P.Parse.parse_filename use_litmus sourcefile in
+    let g = Cfg.of_program program in
+    let module D = (val P.Domain.get domain) in
+    let module Analysis = Interleaving.Make (D) in
+    let data = Analysis.analyze g widening_delay in
+    let module Dot = ExportCfg.Dot (D) in
+    let module Prop = Property.Make (D) in
+    Dot.output_graph (P.Output.get_output outputfile) data g;
+    if Prop.satisfies g data
+    then print_endline "Property verified."
+    else print_endline "Property could not be verified: either it is wrong, or the domain is too imprecise."
+  with
+  | Error.Error e ->
+    prerr_endline @@ Error.to_string e
 
 let main_term =
   let open Param.CommandTerm in
