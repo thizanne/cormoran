8099052e1bd666696a156b6be91c34a8c5447bf3
Thibault Suzanne <thi.suzanne@gmail.com>
2015-09-30 22:03:48 +0200

    Adds Mark.print

--- src/domains/consistency/mark.ml
+++ src/domains/consistency/mark.ml
@@ -23,3 +23,6 @@ let make_update bufs { Domain.var; origin; destinations } =
   (* TODO: assert that var is absent in every buffer of the
      destinations and is present in origin *)
   List.modify_at origin (Buf.remove var) bufs
+
+let print output =
+  List.print (Set.print ~first:"{" ~last:"}" ~sep:"; " Symbol.print) output
