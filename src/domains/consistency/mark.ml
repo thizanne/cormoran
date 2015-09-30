43758b706695a51381b3b3433f6f38bd27cd1387
Thibault Suzanne <thi.suzanne@gmail.com>
2015-09-30 21:11:08 +0200

    Adds Mark consistency domain; WIP

---
+++ src/domains/consistency/mark.ml
@@ -0,0 +1,25 @@
+open Batteries
+open Util
+
+module P = Program
+
+(* A buffer is abstracted by keeping only the information of the variables present in it *)
+module Buf = Symbol.Set
+
+type t = Buf.t list
+
+let compare = List.compare Buf.compare
+
+let tid_is_consistent bufs tid =
+  Buf.is_empty @@ List.nth bufs tid
+
+let write bufs tid var =
+  List.modify_at tid (Buf.add var) bufs
+
+let init prog =
+  List.init (List.length prog.P.threads) (fun _ -> Buf.empty)
+
+let make_update bufs { Domain.var; origin; destinations } =
+  (* TODO: assert that var is absent in every buffer of the
+     destinations and is present in origin *)
+  List.modify_at origin (Buf.remove var) bufs
