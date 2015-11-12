af20431bc0f869c7f58746539206ff35e47d21e4
Thibault Suzanne <thi.suzanne@gmail.com>
2015-11-12 16:57:06 +0100

    Adds function types to Types

--- src/types.ml
+++ src/types.ml
@@ -3,17 +3,19 @@ open Batteries
 type _ t =
   | Int : int t
   | Bool : bool t
+  | Arrow : 'a t * 'b t -> ('a -> 'b) t
 
-let equal : type a b. a t -> b t -> bool =
+let rec equal : type a b. a t -> b t -> bool =
   fun t1 t2 -> match t1, t2 with
     | Int, Int -> true
     | Bool, Bool -> true
-    | Int, Bool -> false
-    | Bool, Int -> false
+    | Arrow (a, b), Arrow (c, d) -> equal a c && equal b d
+    | _, _ -> false
 
-let to_string : type a. a t -> string = function
+let rec to_string : type a. a t -> string = function
   | Int -> "Int"
   | Bool -> "Bool"
+  | Arrow (a, b) -> to_string a ^ " -> " ^ to_string b
 
 type origin =
   | Local
