1249598ac1e423096eb3f969e17224a436de5b0f
Thibault Suzanne <thi.suzanne@gmail.com>
2015-12-07 20:36:47 +0100

    Removes Operations from Cfg interface

--- src/cfg.mli
+++ src/cfg.mli
@@ -8,20 +8,6 @@ module State : sig
   val equal : t -> t -> bool
 end
 
-module Operation : sig
-  type t =
-    | Identity
-    | MFence of Program.thread_id
-    | Filter of Program.var_view Program.condition
-    | Assign of
-        Program.thread_id *
-        Program.var *
-        Program.var Program.expression
-
-  val compare : t -> t -> int
-  val default : t
-end
-
 module G : Graph.Sig.P
   with type V.t = State.t
    and type V.label = State.t
