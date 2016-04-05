5523492b3f0a154f68fbd4c825097eafacbc605a
Thibault Suzanne <thi.suzanne@gmail.com>
2016-04-05 22:17:33 +0200

    WIP modular analysis

--- src/modular.ml
+++ src/modular.ml
@@ -1,3 +1,31 @@
 open Batteries
 
-module Make (D : Domain.Outer)
+module C = Control
+module T = TypedAst
+module TS = Control.ThreadStructure
+
+module type Lol = sig
+  module Interferences : sig
+    type t
+    val equal : t -> t -> bool
+    val init : T.program -> t
+    val update : Source.thread_id -> C.Label.t -> C.Label.t -> Operation.t -> t -> t
+  end
+
+  module ThreadState : Domain.Outer
+
+  val apply : Interferences.t -> ThreadState.t -> ThreadState.t
+end
+
+module OneThread (I : Domain.Inner) = struct
+  (* TODO: better WTO for one-thread graphs *)
+  module Wto = Graph.WeakTopological.Make (TS.Graph)
+
+  module Data = struct
+    type t
+
+    type edge = TS.Graph.edge
+
+
+  end
+end
