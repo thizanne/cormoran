535b6e81d1efe569f18b781f14a48a3230d5201d
Thibault Suzanne <thi.suzanne@gmail.com>
2016-04-06 19:26:06 +0200

    WIP

--- src/modular.ml
+++ src/modular.ml
@@ -4,12 +4,35 @@ module C = Control
 module T = TypedAst
 module TS = Control.ThreadStructure
 
+module type Environment = sig
+  type t
+
+  val init_prog : T.program -> t
+  val init_thread : T.program -> Source.thread_id -> t
+
+  val meet : t -> t -> t
+  val join : t -> t -> t
+  val extend : t -> t
+end
+
+module type Main = sig
+  module Env : Environment
+
+  include Domain.Outer
+
+  val init : Env.t -> t
+end
+
+
+
+
 module type Lol = sig
   module Interferences : sig
     type t
     val equal : t -> t -> bool
-    val init : T.program -> t
-    val update : Source.thread_id -> C.Label.t -> C.Label.t -> Operation.t -> t -> t
+    val init_thread : T.program -> Source.thread_id -> t
+    val update :
+      Source.thread_id -> C.Label.t -> C.Label.t -> Operation.t -> t -> t
   end
 
   module ThreadState : Domain.Outer
