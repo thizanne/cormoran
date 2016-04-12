812c2f455e45dd57413a3d88fb8dcaf8756199e8
Thibault Suzanne <thi.suzanne@gmail.com>
2016-04-12 15:17:19 +0200

    Refactorises Domain interfaces: adds Domain.Common

--- src/domains/domain.ml
+++ src/domains/domain.ml
@@ -2,31 +2,32 @@ open Batteries
 
 module T = TypedAst
 
-module type Outer = sig
+module type Common = sig
   type t
-  val bottom : t
   val is_bottom : t -> bool
   val equal : t -> t -> bool
+  val join : t -> t -> t
+  val print : 'a IO.output -> t -> unit
+end
+
+module type Outer = sig
+  include Common
+  val bottom : t
   val init : T.program -> t
   val transfer : Source.thread_id -> Operation.t -> t -> t
   val meet_cond : T.property_condition -> t -> t
-  val join : t -> t -> t
   val widening : t -> t -> t
-  val print : 'a IO.output -> t -> unit
 end
 
 type 't inner_var = ('t, Sym.t) T.var
 type 't inner_expression = ('t, Sym.t) T.expression
 
 module type Inner = sig
-  type t
-  val is_bottom : t -> bool
-  val equal : t -> t -> bool
+  include Common
   val init :
     (int inner_var * int option) list ->
     (bool inner_var * bool option) list ->
     t
-  val join : t -> t -> t
   val meet : t -> t -> t
   val meet_cons : bool inner_expression -> t -> t
   val widening : t -> t -> t
@@ -35,5 +36,4 @@ module type Inner = sig
   val drop : _ inner_var -> t -> t
   val add : _ inner_var -> t -> t
   val assign_expr : 't inner_var -> 't inner_expression -> t -> t
-  val print : 'a IO.output -> t -> unit
 end
